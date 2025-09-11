5f58ea96eaf9225c9345beada3b7559ed2891d8a
Timofej Gorgun <gorguntimofej@gmail.com>
2025-09-11 15:15:57 +0300

    fix speed test

--- cleanme2/Services/Speedtest/BandwidthTestHost.swift
+++ cleanme2/Services/Speedtest/BandwidthTestHost.swift
@@ -10,14 +10,25 @@ public struct BandwidthTestHost: Codable, Hashable, Identifiable {
     public let hostAddress: String
     public let sponsor: String
     public let distanceKm: Int
-    
+
     private enum CodingKeys: String, CodingKey {
         case url, name, country, sponsor
         case countryCode = "cc"
         case hostAddress = "host"
         case distanceKm = "distance"
     }
-    
+
+    /// Публичный инициализатор для создания экземпляров вручную.
+    public init(url: URL, name: String, country: String, countryCode: String, hostAddress: String, sponsor: String, distanceKm: Int) {
+        self.url = url
+        self.name = name
+        self.country = country
+        self.countryCode = countryCode
+        self.hostAddress = hostAddress
+        self.sponsor = sponsor
+        self.distanceKm = distanceKm
+    }
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         url = try container.decode(URL.self, forKey: .url)
@@ -28,7 +39,7 @@ public struct BandwidthTestHost: Codable, Hashable, Identifiable {
         sponsor = try container.decode(String.self, forKey: .sponsor)
         distanceKm = try container.decode(Int.self, forKey: .distanceKm)
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(url, forKey: .url)
@@ -39,7 +50,7 @@ public struct BandwidthTestHost: Codable, Hashable, Identifiable {
         try container.encode(sponsor, forKey: .sponsor)
         try container.encode(distanceKm, forKey: .distanceKm)
     }
-    
+
     /// URL для HTTP пинга
     public var pingURL: URL? {
         return URL(string: "http://\(hostAddress)")
